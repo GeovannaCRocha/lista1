30b8131d7210e4f97850d79d14d044e9e461c768
GeovannaCosta <158053742+GeovannaCosta@users.noreply.github.com>
2024-06-05 16:07:35 -0300

    Adicionar arquivos de projeto.

---
+++ lista1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace lista1
+{
+    class Projeto
+    {
+        static void Main(string[] args)
+        {
+            /* double salarioPorHora = 35.00;
+               int horasPorDia = 8;
+               double salarioDiario = salarioPorHora * horasPorDia;
+
+               Console.WriteLine("Joaquim deve receber ao final  do dia: R$" + salarioDiario);*/
+
+
+            /* int unidadesPorCaixa = 27;
+             int unidadesCompradas = 16;
+             int bombonsTotais = unidadesCompradas;
+
+             Console.WriteLine("Pamela tem " + bombonsTotais + " bombons.");*/
+
+
+            /*Console.WriteLine("Digite o primeiro valor: ");
+            int valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor; ");
+            int valor2 = int.Parse(Console.ReadLine());
+
+            int soma = valor1 + valor2;
+            Console.WriteLine("A soma dos dois valores é: " +  soma);*/
+
+
+            /*int garrafasVendidas = 300;
+            int garrafasPorCaixa = 20;
+            int caixasUsadas = garrafasVendidas / garrafasPorCaixa;
+
+            Console.WriteLine("A loja usou " +  caixasUsadas + " caixas de água.");*/
+
+
+           /* double precoInstalacao = 300.00;
+            double precoLimpeza = 150.00;
+            double precoManutencao = 499.99;
+            double desconto = 50.00;
+
+            double valorTotalServicos = precoInstalacao + precoLimpeza + precoManutencao;
+            double valorAPagar = valorTotalServicos - desconto;
+
+            Console.WriteLine("O cliente deverá pagar para a empresa: R$ " + valorAPagar);*/
+        }
+    }
+}
